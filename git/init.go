9efd7adbb1aae06d68a6d041176adcc57a706d2d
Fabien Potencier <fabien@potencier.org>
2022-01-01 11:32:33 +0100

    Use main as the default branch when doing git init

--- git/init.go
+++ git/init.go
@@ -22,7 +22,10 @@ package git
 import "bytes"
 
 func Init(dir string, debug bool) (*bytes.Buffer, error) {
-	return doExecGit(dir, []string{"init"}, !debug)
+	if content, err := doExecGit(dir, []string{"init"}, !debug); err != nil {
+		return content, err
+	}
+	return doExecGit(dir, []string{"checkout", "-b", "main"}, !debug)
 }
 
 func AddAndCommit(dir string, files []string, msg string, debug bool) (*bytes.Buffer, error) {
