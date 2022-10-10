364ecc34284df590bb76c46a7ade13f0f4df9af4
Fabien Potencier <fabien@potencier.org>
2022-10-10 11:13:20 +0200

    Update Platform.sh service versions

--- local/platformsh/config.go
+++ local/platformsh/config.go
@@ -188,8 +188,8 @@ var availableServices = []*service{
 	{
 		Type: "mysql",
 		Versions: serviceVersions{
-			Deprecated: []string{"5.5"},
-			Supported:  []string{"10.0", "10.1", "10.2", "10.3", "10.4", "10.5", "10.6"},
+			Deprecated: []string{"5.5", "10.0", "10.1", "10.2"},
+			Supported:  []string{"10.3", "10.4", "10.5", "10.6"},
 		},
 	},
 	{
