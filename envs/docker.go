5b48f670ebec6ec42322499bc8e0d70fe2adbb29
Shyim <s.sayakci@shopware.com>
2023-03-24 14:07:09 +0100

    feat: remove slash on the elasticsearch url

--- envs/docker.go
+++ envs/docker.go
@@ -375,7 +375,7 @@ func (l *Local) dockerServiceToRelationship(client *docker.Client, container typ
 				"host":   host,
 				"ip":     host,
 				"port":   formatDockerPort(p.PublicPort),
-				"path":   "/",
+				"path":   "",
 				"rel":    "elasticsearch",
 				"scheme": "http",
 			}
