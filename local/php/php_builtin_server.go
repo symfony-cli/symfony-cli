92c3ed8b95dcd297318fb277fd18040723896b54
Tomas Kmieliauskas <tomas.kmieliauskas@civinity.com>
2023-07-11 14:36:51 +0300

    Merge  into  earlier in phprouter file

--- local/php/php_builtin_server.go
+++ local/php/php_builtin_server.go
@@ -37,22 +37,23 @@ var phprouter = []byte(`<?php
 
 // Workaround https://bugs.php.net/64566
 if (ini_get('auto_prepend_file') && !in_array(realpath(ini_get('auto_prepend_file')), get_included_files(), true)) {
-    require ini_get('auto_prepend_file');
+	require ini_get('auto_prepend_file');
 }
 
 if (isset($_SERVER['HTTP___SYMFONY_LOCAL_REQUEST_ID__'])) {
-if (file_exists($envFile = __FILE__.'-'.$_SERVER['HTTP___SYMFONY_LOCAL_REQUEST_ID__'].'-env')) {
+	if (file_exists($envFile = __FILE__.'-'.$_SERVER['HTTP___SYMFONY_LOCAL_REQUEST_ID__'].'-env')) {
 		require $envFile;
 	}
 	unset($_SERVER['HTTP___SYMFONY_LOCAL_REQUEST_ID__']);
 }
+
+$_SERVER = array_merge($_SERVER, $_ENV);
 
 if (is_file($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.$_SERVER['SCRIPT_NAME'])) {
 	return false;
 }
 
 $script = $_ENV['APP_FRONT_CONTROLLER'];
-$_SERVER = array_merge($_SERVER, $_ENV);
 $_SERVER['SCRIPT_FILENAME'] = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.$script;
 $_SERVER['SCRIPT_NAME'] = DIRECTORY_SEPARATOR.$script;
 $_SERVER['PHP_SELF'] = DIRECTORY_SEPARATOR.$script;
