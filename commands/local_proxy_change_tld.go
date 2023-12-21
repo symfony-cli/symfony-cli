36428517be9f156bd40185df984b1aaea71996fd
Nihilus118 <ritter.johannes.ma@gmail.com>
2023-12-21 20:16:54 +0100

    remove unused flags

--- commands/local_proxy_change_tld.go
+++ commands/local_proxy_change_tld.go
@@ -31,9 +31,6 @@ var localProxyTLD = &console.Command{
 	Name:     "proxy:change:tld",
 	Aliases:  []*console.Alias{{Name: "proxy:change:tld"}},
 	Usage:    "Change the TLD for the proxy",
-	Flags: []console.Flag{
-		dirFlag,
-	},
 	Args: []*console.Arg{
 		{Name: "tld", Description: "The TLD for the project proxy"},
 	},
