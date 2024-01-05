bdc27baa84bbc5e4ea643051cac5896c44623ce2
Nihilus118 <ritter.johannes.ma@gmail.com>
2024-01-05 12:16:33 +0100

    consistent naming and prevent index out of range error

--- commands/local_proxy_tld.go
+++ commands/local_proxy_tld.go
@@ -32,10 +32,10 @@ import (
 var localProxyTLD = &console.Command{
 	Category: "local",
 	Name:     "proxy:tld",
-	Aliases:  []*console.Alias{{Name: "proxy:change:tld"}},
+	Aliases:  []*console.Alias{{Name: "proxy:tld"}, {Name: "proxy:change:tld"}},
 	Usage:    "Display or change the TLD for the proxy",
 	Args: []*console.Arg{
-		{Name: "tld", Description: "The TLD for the project proxy"},
+		{Name: "tld", Description: "The TLD for the project proxy", Optional: true},
 	},
 	Action: func(c *console.Context) error {
 		homeDir := util.GetHomeDir()
@@ -44,15 +44,17 @@ var localProxyTLD = &console.Command{
 			return err
 		}
 
-		config.TLD = c.Args().Get("tld")
-		if !regexp.MustCompile(`^[a-z]{1,63}$`).MatchString(config.TLD) {
-			return fmt.Errorf("the TLD must only contain lowercase letters")
-		}
-		if err = config.Save(); err != nil {
-			return err
+		if c.Args().Present() {
+			config.TLD = c.Args().Get("tld")
+			if !regexp.MustCompile(`^[a-z]{1,63}$`).MatchString(config.TLD) {
+				return fmt.Errorf("the TLD must only contain lowercase letters")
+			}
+			if err = config.Save(); err != nil {
+				return err
+			}
 		}
 
-		terminal.Printfln("<info>The proxy is now configured with the following TLD: %s</>", config.TLD)
+		terminal.Printfln("<info>The proxy is configured with the following TLD: %s</>", config.TLD)
 		return nil
 	},
 }
