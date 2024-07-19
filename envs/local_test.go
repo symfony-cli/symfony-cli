be2a0fa3241782472ab4e4054ec3ff08f1393618
Kevin Studer <kevin.studer@unibe.ch>
2024-07-19 15:07:22 +0200

    Implement test which checks env relationships

--- envs/local_test.go
+++ envs/local_test.go
@@ -20,11 +20,20 @@
 package envs
 
 import (
+	"fmt"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"context"
+
+	docker "github.com/docker/docker/client"
 	"github.com/mitchellh/go-homedir"
 	"github.com/symfony-cli/symfony-cli/local/platformsh"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 	. "gopkg.in/check.v1"
 )
 
@@ -97,4 +106,262 @@ func (s *LocalSuite) TestRelationships(c *C) {
 		"PGUSER":                 "main",
 		"PGHOST":                 "127.0.0.1",
 	})
+}
+
+func (s *LocalSuite) TestGenericContainer(c *C) {
+	ctx := context.Background()
+
+	composeProject := os.Getenv("COMPOSE_PROJECT_NAME")
+	defer os.Setenv("COMPOSE_PROJECT_NAME", composeProject)
+
+	os.Setenv("COMPOSE_PROJECT_NAME", "TestGenericContainer")
+
+	cmd := []string{"--providers.docker=false", "--entryPoints.web.address=:3000"}
+	labels := testcontainers.GenericLabels()
+	labels["com.docker.compose.project"] = "testgenericcontainer"
+	labels["com.docker.compose.service"] = "generic"
+	req := testcontainers.ContainerRequest{
+		Image:        "traefik:v3.1",
+		ExposedPorts: []string{"3000/tcp"},
+		WaitingFor:   wait.ForListeningPort("3000/tcp"),
+		Labels:       labels,
+		Cmd:          cmd,
+	}
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		c.Errorf("Could not start container: %s", err)
+		c.FailNow()
+	}
+
+	host := os.Getenv(docker.EnvOverrideHost)
+	if host == "" || strings.HasPrefix(host, "unix://") {
+		host = "127.0.0.1"
+	} else {
+		u, err := url.Parse(host)
+		if err != nil {
+			host = "127.0.0.1"
+		} else {
+			host = u.Hostname()
+		}
+	}
+
+	mappedPort, err := container.MappedPort(ctx, "3000/tcp")
+	if err != nil {
+		c.Errorf("Could not get mapped port of container: %s", err)
+		c.FailNow()
+	}
+
+	c.Assert(extractRelationshipsEnvs(&Local{}), DeepEquals, Envs{
+		"GENERIC_HOST":   host,
+		"GENERIC_IP":     host,
+		"GENERIC_PORT":   mappedPort.Port(),
+		"GENERIC_URL":    fmt.Sprintf("tcp://127.0.0.1:%s", mappedPort.Port()),
+		"GENERIC_SCHEME": "tcp",
+	})
+
+	defer func() {
+		if err := container.Terminate(ctx); err != nil {
+			c.Errorf("Could not stop redis: %s", err)
+			c.FailNow()
+		}
+	}()
+}
+
+func (s *LocalSuite) TestGenericContainerWithServicePrefix(c *C) {
+	ctx := context.Background()
+
+	composeProject := os.Getenv("COMPOSE_PROJECT_NAME")
+	defer os.Setenv("COMPOSE_PROJECT_NAME", composeProject)
+
+	os.Setenv("COMPOSE_PROJECT_NAME", "TestGenericContainer")
+
+	cmd := []string{"--providers.docker=false", "--entryPoints.web.address=:3000"}
+	labels := testcontainers.GenericLabels()
+	labels["com.docker.compose.project"] = "testgenericcontainer"
+	labels["com.symfony.server.service-prefix"] = "CUSTOM"
+	req := testcontainers.ContainerRequest{
+		Image:        "traefik:v3.1",
+		ExposedPorts: []string{"3000/tcp"},
+		WaitingFor:   wait.ForListeningPort("3000/tcp"),
+		Labels:       labels,
+		Cmd:          cmd,
+	}
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		c.Errorf("Could not start container: %s", err)
+		c.FailNow()
+	}
+
+	host := os.Getenv(docker.EnvOverrideHost)
+	if host == "" || strings.HasPrefix(host, "unix://") {
+		host = "127.0.0.1"
+	} else {
+		u, err := url.Parse(host)
+		if err != nil {
+			host = "127.0.0.1"
+		} else {
+			host = u.Hostname()
+		}
+	}
+
+	mappedPort, err := container.MappedPort(ctx, "3000/tcp")
+	if err != nil {
+		c.Errorf("Could not get mapped port of container: %s", err)
+		c.FailNow()
+	}
+
+	c.Assert(extractRelationshipsEnvs(&Local{}), DeepEquals, Envs{
+		"CUSTOM_HOST":   host,
+		"CUSTOM_IP":     host,
+		"CUSTOM_PORT":   mappedPort.Port(),
+		"CUSTOM_URL":    fmt.Sprintf("tcp://127.0.0.1:%s", mappedPort.Port()),
+		"CUSTOM_SCHEME": "tcp",
+	})
+
+	defer func() {
+		if err := container.Terminate(ctx); err != nil {
+			c.Errorf("Could not stop redis: %s", err)
+			c.FailNow()
+		}
+	}()
+}
+
+func (s *LocalSuite) TestRedisContainer(c *C) {
+	ctx := context.Background()
+
+	composeProject := os.Getenv("COMPOSE_PROJECT_NAME")
+	defer os.Setenv("COMPOSE_PROJECT_NAME", composeProject)
+
+	os.Setenv("COMPOSE_PROJECT_NAME", "TestRedisContainer")
+
+	labels := testcontainers.GenericLabels()
+	labels["com.docker.compose.project"] = "testrediscontainer"
+	labels["com.docker.compose.service"] = "redis"
+	req := testcontainers.ContainerRequest{
+		Image:        "redis:latest",
+		ExposedPorts: []string{"6379/tcp"},
+		WaitingFor:   wait.ForListeningPort("6379/tcp"),
+		Labels:       labels,
+	}
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		c.Errorf("Could not start container: %s", err)
+		c.FailNow()
+	}
+
+	host := os.Getenv(docker.EnvOverrideHost)
+	if host == "" || strings.HasPrefix(host, "unix://") {
+		host = "127.0.0.1"
+	} else {
+		u, err := url.Parse(host)
+		if err != nil {
+			host = "127.0.0.1"
+		} else {
+			host = u.Hostname()
+		}
+	}
+
+	mappedPort, err := container.MappedPort(ctx, "6379/tcp")
+	if err != nil {
+		c.Errorf("Could not get mapped port of container: %s", err)
+		c.FailNow()
+	}
+
+	c.Assert(extractRelationshipsEnvs(&Local{}), DeepEquals, Envs{
+		"REDIS_HOST":   host,
+		"REDIS_PORT":   mappedPort.Port(),
+		"REDIS_URL":    fmt.Sprintf("redis://127.0.0.1:%s", mappedPort.Port()),
+		"REDIS_SCHEME": "redis",
+	})
+
+	defer func() {
+		if err := container.Terminate(ctx); err != nil {
+			c.Errorf("Could not stop redis: %s", err)
+			c.FailNow()
+		}
+	}()
+}
+
+func (s *LocalSuite) TestDatabaseContainer(c *C) {
+	ctx := context.Background()
+
+	composeProject := os.Getenv("COMPOSE_PROJECT_NAME")
+	defer os.Setenv("COMPOSE_PROJECT_NAME", composeProject)
+
+	os.Setenv("COMPOSE_PROJECT_NAME", "TestDatabaseContainer")
+
+	labels := testcontainers.GenericLabels()
+	labels["com.docker.compose.project"] = "testdatabasecontainer"
+	labels["com.docker.compose.service"] = "database"
+	req := testcontainers.ContainerRequest{
+		Image:        "postgres:16.3-alpine",
+		ExposedPorts: []string{"5432/tcp"},
+		WaitingFor:   wait.ForListeningPort("5432/tcp"),
+		Labels:       labels,
+		Env: map[string]string{
+			"POSTGRES_USER":     "app",
+			"POSTGRES_PASSWORD": "password",
+		},
+	}
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		c.Errorf("Could not start container: %s", err)
+		c.FailNow()
+	}
+
+	host := os.Getenv(docker.EnvOverrideHost)
+	if host == "" || strings.HasPrefix(host, "unix://") {
+		host = "127.0.0.1"
+	} else {
+		u, err := url.Parse(host)
+		if err != nil {
+			host = "127.0.0.1"
+		} else {
+			host = u.Hostname()
+		}
+	}
+
+	mappedPort, err := container.MappedPort(ctx, "5432/tcp")
+	if err != nil {
+		c.Errorf("Could not get mapped port of container: %s", err)
+		c.FailNow()
+	}
+
+	c.Assert(extractRelationshipsEnvs(&Local{}), DeepEquals, Envs{
+		"PGHOST":            host,
+		"PGUSER":            "app",
+		"PGDATABASE":        "app",
+		"PGPASSWORD":        "password",
+		"PGPORT":            mappedPort.Port(),
+		"DATABASE_DRIVER":   "postgres",
+		"DATABASE_VERSION":  "16.3",
+		"DATABASE_USERNAME": "app",
+		"DATABASE_USER":     "app",
+		"DATABASE_NAME":     "app",
+		"DATABASE_DATABASE": "app",
+		"DATABASE_PASSWORD": "password",
+		"DATABASE_SERVER":   fmt.Sprintf("postgres://127.0.0.1:%s", mappedPort.Port()),
+		"DATABASE_HOST":     host,
+		"DATABASE_PORT":     mappedPort.Port(),
+		"DATABASE_URL":      fmt.Sprintf("postgres://%s:%s@127.0.0.1:%s/%s%s", "app", "password", mappedPort.Port(), "app", "?sslmode=disable&charset=utf8&serverVersion=16.3"),
+	})
+
+	defer func() {
+		if err := container.Terminate(ctx); err != nil {
+			c.Errorf("Could not stop redis: %s", err)
+			c.FailNow()
+		}
+	}()
 }
--- go.mod
+++ go.mod
@@ -36,9 +36,16 @@ require (
 )
 
 require (
+	dario.cat/mergo v1.0.0 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/Microsoft/hcsshim v0.11.5 // indirect
 	github.com/agext/levenshtein v1.2.3 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/containerd/containerd v1.7.18 // indirect
+	github.com/containerd/errdefs v0.1.0 // indirect
 	github.com/containerd/log v0.1.0 // indirect
+	github.com/cpuguy83/dockercfg v0.3.1 // indirect
 	github.com/distribution/reference v0.6.0 // indirect
 	github.com/docker/go-connections v0.5.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
@@ -46,14 +53,19 @@ require (
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/btree v1.1.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/imdario/mergo v0.3.16 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
@@ -61,17 +73,27 @@ require (
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/moby/patternmatcher v0.6.0 // indirect
+	github.com/moby/sys/sequential v0.5.0 // indirect
+	github.com/moby/sys/user v0.1.0 // indirect
 	github.com/moby/term v0.5.0 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0 // indirect
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/shirou/gopsutil/v3 v3.23.12 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/testcontainers/testcontainers-go v0.32.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.53.0 // indirect
 	go.opentelemetry.io/otel v1.28.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.28.0 // indirect
@@ -84,6 +106,9 @@ require (
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/term v0.22.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240701130421-f6361c86f094 // indirect
+	google.golang.org/grpc v1.64.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	howett.net/plist v1.0.1 // indirect
