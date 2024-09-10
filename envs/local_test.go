f022c31b2b6620434d0d230374620f2c9096ff1c
Kevin Studer <kevin.studer@unibe.ch>
2024-09-10 10:20:58 +0200

    Added tests for env extraction in docker

--- envs/local_test.go
+++ envs/local_test.go
@@ -20,11 +20,22 @@
 package envs
 
 import (
+	"fmt"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"context"
+
+	"github.com/docker/compose/v2/pkg/api"
+	docker "github.com/docker/docker/client"
 	"github.com/mitchellh/go-homedir"
+	"github.com/stretchr/testify/require"
 	"github.com/symfony-cli/symfony-cli/local/platformsh"
+
+	tc "github.com/testcontainers/testcontainers-go/modules/compose"
+	"github.com/testcontainers/testcontainers-go/wait"
 	. "gopkg.in/check.v1"
 )
 
@@ -97,4 +108,215 @@ func (s *LocalSuite) TestRelationships(c *C) {
 		"PGUSER":                 "main",
 		"PGHOST":                 "127.0.0.1",
 	})
+}
+
+func (s *LocalSuite) TestGenericContainer(c *C) {
+	os.Setenv("COMPOSE_PROJECT_NAME", "generic")
+	identifier := tc.StackIdentifier("generic")
+	compose, err := tc.NewDockerComposeWith(tc.WithStackFiles("./testdata/local_project/generic_compose.yaml"), identifier)
+	require.NoError(c, err, "NewDockerComposeAPIWith()")
+
+	ctx := context.Background()
+
+	require.NoError(c, compose.Up(ctx, tc.WithRecreate(api.RecreateNever), tc.Wait(true)), "compose.Up()")
+
+	compose.WaitForService("generic", wait.ForListeningPort("3000/tcp"))
+
+	host := os.Getenv(docker.EnvOverrideHost)
+	if host == "" || strings.HasPrefix(host, "unix://") {
+		host = "127.0.0.1"
+	} else {
+		u, err := url.Parse(host)
+		if err != nil {
+			host = "127.0.0.1"
+		} else {
+			host = u.Hostname()
+		}
+	}
+
+	container, err := compose.ServiceContainer(ctx, "generic")
+	if err != nil {
+		c.Errorf("Could not get service with name %s", "generic")
+		c.FailNow()
+	}
+
+	mappedPort, err := container.MappedPort(ctx, "3000/tcp")
+
+	if err != nil {
+		c.Errorf("Could not get mapped port of container: %s", err)
+		c.FailNow()
+	}
+
+	c.Assert(extractRelationshipsEnvs(&Local{}), DeepEquals, Envs{
+		"GENERIC_HOST":   host,
+		"GENERIC_IP":     host,
+		"GENERIC_PORT":   mappedPort.Port(),
+		"GENERIC_URL":    fmt.Sprintf("tcp://127.0.0.1:%s", mappedPort.Port()),
+		"GENERIC_SCHEME": "tcp",
+	})
+
+	defer func() {
+		require.NoError(c, compose.Down(context.Background(), tc.RemoveOrphans(true), tc.RemoveImagesLocal), "compose.Down()")
+	}()
+}
+
+func (s *LocalSuite) TestGenericContainerWithServicePrefix(c *C) {
+	os.Setenv("COMPOSE_PROJECT_NAME", "genericcustom")
+	identifier := tc.StackIdentifier("genericcustom")
+	compose, err := tc.NewDockerComposeWith(tc.WithStackFiles("./testdata/local_project/generic_service_compose.yaml"), identifier)
+	require.NoError(c, err, "NewDockerComposeAPIWith()")
+
+	ctx := context.Background()
+
+	require.NoError(c, compose.Up(ctx, tc.WithRecreate(api.RecreateNever), tc.Wait(true)), "compose.Up()")
+
+	compose.WaitForService("generic", wait.ForListeningPort("3000/tcp"))
+
+	host := os.Getenv(docker.EnvOverrideHost)
+	if host == "" || strings.HasPrefix(host, "unix://") {
+		host = "127.0.0.1"
+	} else {
+		u, err := url.Parse(host)
+		if err != nil {
+			host = "127.0.0.1"
+		} else {
+			host = u.Hostname()
+		}
+	}
+
+	container, err := compose.ServiceContainer(ctx, "generic")
+	if err != nil {
+		c.Errorf("Could not get service with name %s", "generic")
+		c.FailNow()
+	}
+
+	mappedPort, err := container.MappedPort(ctx, "3000/tcp")
+
+	if err != nil {
+		c.Errorf("Could not get mapped port of container: %s", err)
+		c.FailNow()
+	}
+
+	c.Assert(extractRelationshipsEnvs(&Local{}), DeepEquals, Envs{
+		"CUSTOM_HOST":   host,
+		"CUSTOM_IP":     host,
+		"CUSTOM_PORT":   mappedPort.Port(),
+		"CUSTOM_URL":    fmt.Sprintf("tcp://127.0.0.1:%s", mappedPort.Port()),
+		"CUSTOM_SCHEME": "tcp",
+	})
+
+	defer func() {
+		require.NoError(c, compose.Down(context.Background(), tc.RemoveOrphans(true), tc.RemoveImagesLocal), "compose.Down()")
+	}()
+}
+
+func (s *LocalSuite) TestRedisContainer(c *C) {
+
+	os.Setenv("COMPOSE_PROJECT_NAME", "redis")
+	identifier := tc.StackIdentifier("redis")
+	compose, err := tc.NewDockerComposeWith(tc.WithStackFiles("./testdata/local_project/redis_compose.yaml"), identifier)
+	require.NoError(c, err, "NewDockerComposeAPIWith()")
+
+	ctx := context.Background()
+
+	require.NoError(c, compose.Up(ctx, tc.WithRecreate(api.RecreateNever), tc.Wait(true)), "compose.Up()")
+
+	compose.WaitForService("redis", wait.ForListeningPort("6379/tcp"))
+
+	host := os.Getenv(docker.EnvOverrideHost)
+	if host == "" || strings.HasPrefix(host, "unix://") {
+		host = "127.0.0.1"
+	} else {
+		u, err := url.Parse(host)
+		if err != nil {
+			host = "127.0.0.1"
+		} else {
+			host = u.Hostname()
+		}
+	}
+
+	container, err := compose.ServiceContainer(ctx, "redis")
+	if err != nil {
+		c.Errorf("Could not get service with name %s", "redis")
+		c.FailNow()
+	}
+
+	mappedPort, err := container.MappedPort(ctx, "6379/tcp")
+
+	if err != nil {
+		c.Errorf("Could not get mapped port of container: %s", err)
+		c.FailNow()
+	}
+
+	c.Assert(extractRelationshipsEnvs(&Local{}), DeepEquals, Envs{
+		"REDIS_HOST":   host,
+		"REDIS_PORT":   mappedPort.Port(),
+		"REDIS_URL":    fmt.Sprintf("redis://127.0.0.1:%s", mappedPort.Port()),
+		"REDIS_SCHEME": "redis",
+	})
+
+	defer func() {
+		require.NoError(c, compose.Down(context.Background(), tc.RemoveOrphans(true), tc.RemoveImagesLocal), "compose.Down()")
+	}()
+}
+
+func (s *LocalSuite) TestDatabaseContainer(c *C) {
+	os.Setenv("COMPOSE_PROJECT_NAME", "postgres")
+	identifier := tc.StackIdentifier("postgres")
+	compose, err := tc.NewDockerComposeWith(tc.WithStackFiles("./testdata/local_project/postgres_compose.yaml"), identifier)
+	require.NoError(c, err, "NewDockerComposeAPIWith()")
+
+	ctx := context.Background()
+
+	require.NoError(c, compose.Up(ctx, tc.WithRecreate(api.RecreateNever), tc.Wait(true)), "compose.Up()")
+
+	compose.WaitForService("database", wait.ForListeningPort("5432/tcp"))
+
+	host := os.Getenv(docker.EnvOverrideHost)
+	if host == "" || strings.HasPrefix(host, "unix://") {
+		host = "127.0.0.1"
+	} else {
+		u, err := url.Parse(host)
+		if err != nil {
+			host = "127.0.0.1"
+		} else {
+			host = u.Hostname()
+		}
+	}
+
+	container, err := compose.ServiceContainer(ctx, "database")
+	if err != nil {
+		c.Errorf("Could not get service with name %s", "database")
+		c.FailNow()
+	}
+
+	mappedPort, err := container.MappedPort(ctx, "5432/tcp")
+
+	if err != nil {
+		c.Errorf("Could not get mapped port of container: %s", err)
+		c.FailNow()
+	}
+
+	c.Assert(extractRelationshipsEnvs(&Local{}), DeepEquals, Envs{
+		"PGHOST":            host,
+		"PGUSER":            "app",
+		"PGDATABASE":        "app",
+		"PGPASSWORD":        "password",
+		"PGPORT":            mappedPort.Port(),
+		"DATABASE_DRIVER":   "postgres",
+		"DATABASE_VERSION":  "16.3",
+		"DATABASE_USERNAME": "app",
+		"DATABASE_USER":     "app",
+		"DATABASE_NAME":     "app",
+		"DATABASE_DATABASE": "app",
+		"DATABASE_PASSWORD": "password",
+		"DATABASE_SERVER":   fmt.Sprintf("postgres://127.0.0.1:%s", mappedPort.Port()),
+		"DATABASE_HOST":     host,
+		"DATABASE_PORT":     mappedPort.Port(),
+		"DATABASE_URL":      fmt.Sprintf("postgres://%s:%s@127.0.0.1:%s/%s%s", "app", "password", mappedPort.Port(), "app", "?sslmode=disable&charset=utf8&serverVersion=16.3"),
+	})
+
+	defer func() {
+		require.NoError(c, compose.Down(context.Background(), tc.RemoveOrphans(true), tc.RemoveImagesLocal), "compose.Down()")
+	}()
 }
