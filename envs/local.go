f11e67e0916cdcd15a04df9d11365a3b32511d0c
Fabien Potencier <fabien@potencier.org>
2024-11-28 17:56:12 +0100

    Fix path on local envs

--- envs/local.go
+++ envs/local.go
@@ -48,7 +48,7 @@ func NewLocal(path string, debug bool) (*Local, error) {
 		return nil, errors.WithStack(err)
 	}
 	return &Local{
-		Dir:   path,
+		Dir:   guessProjectDir(path),
 		Debug: debug,
 	}, nil
 }
@@ -282,4 +282,21 @@ func (l *Local) webServer() Envs {
 	}
 
 	return env
+}
+
+func guessProjectDir(dir string) string {
+	for {
+		f, err := os.Stat(filepath.Join(dir, ".git"))
+		if err == nil && f.IsDir() {
+			return dir
+		}
+
+		upDir := filepath.Dir(dir)
+		if upDir == dir || upDir == "." {
+			break
+		}
+		dir = upDir
+	}
+
+	return ""
 }
