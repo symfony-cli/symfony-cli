1dbd0078321e23128fdfda5cddf9175dcad96b92
Lewis Brown <lewisclbrown@gmail.com>
2025-03-13 16:58:03 -0500

    Modified logic to conditionally use console ips over hardcoded string array
    When running without --ips="..." flag, ips is "" rather than being sourced by the DefaultText attribute as previously thought. I restored the previous hardcoded string array and use the ips value if it evaluates to as truthy.

--- commands/local_server_ca_install.go
+++ commands/local_server_ca_install.go
@@ -75,8 +75,11 @@ var localServerCAInstallCmd = &console.Command{
 		p12 := filepath.Join(homeDir, "certs", "default.p12")
 		if _, err := os.Stat(p12); os.IsNotExist(err) {
 			terminal.Println("Generating a default certificate for HTTPS support")
-			ips := strings.Split(c.String("ips"), ",")
-			err := ca.MakeCert(p12, ips)
+			if c.String("ips") {
+				err := ca.MakeCert(p12, strings.Split(c.String("ips"), ","))
+			} else {
+				err := ca.MakeCert(p12, []string{"localhost", "127.0.0.1", "::1"})
+			}
 			if err != nil {
 				return errors.Wrap(err, "failed to generate a default certificate")
 			}
